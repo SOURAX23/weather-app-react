bcaa6298aaa39cf940e7e4795073a5e752da0b40
SOURAX23 <sourabsingh371@gmail.com>
2026-07-10 23:57:52 +0530

    Initial Commit

---
+++ src/SearchBox.jsx
@@ -0,0 +1,64 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
+import "./search.css"
+
+export default function SearchBox({updateInfo}){
+    let [city,setCity]=useState("");
+    let [error,setError]=useState(false);
+    const API_URL="https://api.openweathermap.org/data/2.5/weather"
+    const API_KEY="b72155e79309ae9b2dce082764460db2";
+
+    let getInfo=async()=>{
+        try {
+            let response= await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonres=await response.json();
+        let result={
+            city:city,
+            temp:jsonres.main.temp,
+            tempMin:jsonres.main.temp_min,
+            tempMax:jsonres.main.temp_max,
+            humidity:jsonres.main.humidity,
+            feelsLike:jsonres.main.feels_like,
+            weather:jsonres.weather[0].description,
+        };
+        console.log(result);
+        return result;
+        } catch (error) {
+            throw error;
+        }
+        
+    };
+    
+    let handleChange=(e)=>{
+        setCity(e.target.value);
+    }
+    let handleSumbit=async(e)=>{
+        try {
+            e.preventDefault();
+            setError(false);
+        console.log( city);
+        setCity("");
+        let newInfo=await getInfo();
+        updateInfo(newInfo);
+        } catch (error) {
+            setError(true)
+        }
+        
+    }
+
+    return (
+        <div className='sbox'>
+        <h3>Search for weather</h3>
+        <form onSubmit={handleSumbit}>
+        <TextField id="city" label="City Name" variant="outlined" value={city} onChange={handleChange}/>
+        <br />
+        <Button variant="contained" endIcon={<SearchIcon />} type='sumbit'>
+        Send
+      </Button>
+      { error ? <p>No such place exists</p>:''}
+      </form>
+        </div>
+    )
+}
---
+++ src/infoBox.jsx
@@ -0,0 +1,58 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import CloudIcon from '@mui/icons-material/Cloud';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import FoggyIcon from '@mui/icons-material/Foggy';
+import "./ibox.css"
+
+export default function InfoBox({info}){
+    const init_url="https://images.unsplash.com/photo-1534088568595-a066f410bcda?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YnJva2VuJTIwY2xvdWRzfGVufDB8fDB8fHww";
+
+    let hot_url='https://images.unsplash.com/photo-1504370805625-d32c54b16100?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D';
+    let cold_url='https://images.unsplash.com/photo-1674407866481-a39b2239f771?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D';
+    let rain_url='https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFpbnklMjB3ZWF0aGVyfGVufDB8fDB8fHww';
+
+    
+   
+    return(
+        <div className="InfoBox">
+        <div className="container">
+        <Card sx={{ maxWidth: 345, 
+                    borderRadius: 4,
+                    boxShadow: 8,
+                  }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={info.humidity>80 ? rain_url : info.temp>15 ? hot_url : cold_url}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.city } 
+          {info.weather.includes("clear") && <WbSunnyIcon />}
+          {info.weather.includes("cloud") && <CloudIcon />}
+          {info.weather.includes("rain") && <GrainIcon />}
+          {info.weather.includes("thunder") && <ThunderstormIcon />}
+          {info.weather.includes("snow") && <AcUnitIcon />}
+          {info.weather.includes("mist") && <FoggyIcon />}
+          {info.weather.includes("haze") && <FoggyIcon />}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }} component={'span'}>
+            <p>Temperature:{info.temp}&deg;C</p>
+            <p>Humidity:{info.humidity}</p>
+            <p>Max Temperature:{info.tempMax}</p>
+            <p>Min Temperature:{info.tempMin}</p>
+            <p>
+                The weather can described as <i>{info.weather}</i> and feels like {info.feelsLike}&deg;C
+            </p>
+        </Typography>
+      </CardContent>
+    </Card>
+        </div>
+        </div>
+    )
+}
